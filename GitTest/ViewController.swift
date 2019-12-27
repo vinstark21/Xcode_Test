6b02427d517095c3e49c589b3b242aa43c97db8c
vinstark21 <vinstark@finports.com>
2019-12-27 15:20:39 +0900

    Initial Commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by finports_dev on 2019/12/27.
+//  Copyright © 2019 finports_dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
